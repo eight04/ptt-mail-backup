086cd452dd72e43d888173fc9e722c751e40765e
eight <eight04@gmail.com>
2018-06-11 20:39:17 +0800

    WIP: grab long line

--- ptt_mail_backup/__init__.py
+++ ptt_mail_backup/__init__.py
@@ -4,16 +4,16 @@ from getpass import getpass
 import logging
 import math
 from pathlib import Path
-from pdb import set_trace
-from pyperclip import copy
+# from pdb import set_trace
 import re
 
 # from PTTLibrary import PTT
 from paramiko.client import SSHClient, AutoAddPolicy
 # from ptt_article_parser import strip_color
-from uao import register_uao
 import pyte
 import pyte.graphics
+from pyperclip import copy
+from uao import register_uao
 
 __version__ = "0.0.0"
 
@@ -93,32 +93,15 @@ def colored_sequence(chars):
     if not color.is_default():
         yield ColorState.RESET
         
-class Article:
-    """Article composer. Compose multiple screens into an article."""
-    foot_rx = re.compile(r"(?:(\d+)~(\d+)\s*欄.+?)?(\d+)~(\d+)\s*行".encode("big5-uao"))
-    
-    def __init__(self):
-        self.lines = []
-        
-    def draw_char(self, line_no, col_no, char):
-        if col_no >= len(self.lines[line_no]):
-            for i in range(line_no - len(self.lines[line_no]) + 1):
-                self.lines[line_no].append(None)
-                
-        if self.lines[line_no][col_no] is None:
-            self.lines[line_no][col_no] = char
-        
-    def draw_line(line_no, col_start, line):
-        if line_no > len(self.lines):
-            for i in range(line_no - len(self.lines)):
-                self.lines.append([])
-            
+class ArticleScreenLine:
+    def __init__(self, line, line_no, col_start):
         skip_start = 0
         if is_truncated(line[0]):
             if line[1].data == "?":
                 skip_start = 2
             else:
                 skip_start = 1
+                
         skip_end = 0
         if is_truncated(line[78]) and is_default(line[79]):
             if line[77].data == "?":
@@ -126,45 +109,86 @@ class Article:
             else:
                 skip_end = 2
                 
-        if line_no == len(self.lines) and col_start == 0:
-            # no need to draw char
-            self.lines.append(line)
-            return
-            
-        col_start += skip_start
-        line = line[skip_start:len(line) - skip_end]
-        for col_no, char in enumerate(col_start, line):
-            self.draw_char(line_no, col_no, char)
+        self.line_no = line_no
+        self.col_no = col_start + skip_start
+        self.chars = line[skip_start:len(line) - skip_end]
+        self.left_truncated = bool(skip_start)
+        self.right_truncated = bool(skip_end)
         
-    def add_screen(self, lines):
+class ArticleScreen:
+    foot_rx = re.compile(r"(?:(\d+)~(\d+)\s*欄.+?)?(\d+)~(\d+)\s*行".encode("big5-uao"))
+    
+    def __init__(self, lines):
         lines = list(lines)
-        # set_trace()
+        
         result = self.foot_rx.search(lines[-1]).groups()
         # make number zero-based
         col_start, col_end, line_start, line_end = [
-            int(n) - 1 for n in result if n is not None else None
+            int(n) - 1 if n is not None else None for n in result
         ]
         if not col_start:
             col_start = 0
         if not col_end:
             col_end = 77
             
-        assert line_start <= len(self.lines)
-        for line_no, line in enumerate(lines[:-1], line_start):
-            self.draw_line(line_no, col_start, line)
+        self.line_start = line_start
+        self.line_end = line_end
+        self.col_start = col_start
+        self.col_end = col_end
+        
+        self.lines = [
+            ArticleScreenLine(line, line_no, col_start)
+            for line_no, line in enumerate(lines[:-1], line_start)
+        ]
+        
+def is_default(char):
+    return not char.bold and char.fg == "default" and char.bg == "default"
+    
+def is_truncated(char):
+    return char.bold and (char.data == "<" or char.data == ">")
+        
+class Article:
+    """Article composer. Compose multiple screens into an article."""
+    def __init__(self):
+        self.lines = []
+        
+    def draw_char(self, line_no, col_no, char):
+        if col_no >= len(self.lines[line_no]):
+            self.lines[line_no].extend([None] * (line_no - len(self.lines[line_no]) + 1))
+                
+        if self.lines[line_no][col_no] is None:
+            self.lines[line_no][col_no] = char
+        
+    def draw_line(self, line):
+        if line.line_no > len(self.lines):
+            for _i in range(line.line_no - len(self.lines)):
+                self.lines.append([])
+                
+        if line.line_no == len(self.lines) and line.col_no == 0:
+            # no need to draw char
+            self.lines.append(line.chars)
+            return
             
-        return col_start, col_end, line_start, line_end
+        for col_no, char in enumerate(line.col_no, line.chars):
+            self.draw_char(line.line_no, col_no, char)
+        
+    def add_screen(self, lines):
+        screen = ArticleScreen(lines)
+        assert screen.line_start <= len(self.lines)
+        for line in screen.lines:
+            self.draw_line(line)
+        return screen
         
     def to_bytes(self):
         return b"\r\n".join(b"".join(colored_sequence(l)).rstrip() for l in self.lines)
         
 def is_no(text):
-    return bool(re.match("\s*(n|no)\s*", text, re.I))
+    return bool(re.match(r"\s*(n|no)\s*", text, re.I))
     
 def no_c1_pattern(rx):
-	# pyte uses a single backslash to escape every characters
-	pattern = re.sub(r"\\[\x9b\x9d]", "", rx.pattern)
-	return re.compile(pattern)
+    # pyte uses a single backslash to escape every characters
+    pattern = re.sub(r"\\[\x9b\x9d]", "", rx.pattern)
+    return re.compile(pattern)
         
 class PTTBot:
     def __init__(self, user=None, password=None):
@@ -176,6 +200,8 @@ class PTTBot:
         self.screen = pyte.Screen(80, 24)
         self.stream = pyte.ByteStream(self.screen)
         self.stream.select_other_charset("@")
+        # pylint: disable=protected-access
+        # https://github.com/selectel/pyte/issues/118
         self.stream._text_pattern = no_c1_pattern(pyte.ByteStream._text_pattern)
         self.article_configured = False
         
@@ -194,7 +220,7 @@ class PTTBot:
         log.info("start login")
         self.send(self.user + "\r" + self.password + "\r")
         self.unt("任意鍵", on_data=self.handle_login)
-        log.info("login success".format(self.user))
+        log.info("%s login success", self.user)
         self.send("qqq")
         # set_trace()
         self.unt("主功能表")
@@ -219,16 +245,18 @@ class PTTBot:
         self.client.close()
     
     def unt(self, needle, on_data=None):
-        if not callable(needle):
-            def needle(data, test=needle.encode("big5-uao")):
-                return test in data 
+        if callable(needle):
+            should_stop = needle
+        else:
+            def should_stop(data, test=needle.encode("big5-uao")):
+                return test in data
                 
         while True:
             data = self.channel.recv(math.inf)
             if on_data:
                 on_data(data)
             self.stream.feed(data)
-            if needle(data):
+            if should_stop(data):
                 return
         
     def send(self, data):
@@ -268,7 +296,7 @@ class PTTBot:
                     break
         self.unt("呼叫小天使", on_data=on_data)
         self.send("q")
-        log.info("get last index success: {}".format(last_index))
+        log.info("get last index success: %s", last_index)
         return last_index
         
     def lines(self, reverse=False, color=False, raw=False):
@@ -278,7 +306,7 @@ class PTTBot:
             it = range(self.screen.lines)
         for i in it:
             if raw:
-                yield selt.get_raw_line(i)
+                yield self.get_raw_line(i)
             else:
                 yield self.get_line(i, color=color)
                 
@@ -300,61 +328,98 @@ class PTTBot:
         self.article_configured = True
         self.send("owml ")
         rx = re.compile(r"\s*瀏覽\s*第\s*\d+/\d+\s*頁".encode("big5-uao"))
-        def is_article(data):
+        def is_article(_data):
             last_line = self.get_line(self.screen.lines - 1)
             return rx.match(last_line)
         self.unt(is_article)
         
+    first_col_needle = "目前顯示:".encode("big5-uao")
+    def on_first_col(self, _data):
+        return self.first_col_needle in self.get_line(-1)
+        
+    def on_col(self, col_no):
+        needle = "{}~{} 欄".format(col_no + 1, col_no + 78)
+        def callback(_data):
+            return needle in self.get_line(-1)
+        return callback
+        
     def get_article(self, index):
-        log.info("get {}th article".format(index))
+        log.info("get %sth article", index)
         self.send(str(index) + "\r\r")
+        
         is_animated = False
-        def on_data(data):
+        def handle_animated(data):
             nonlocal is_animated
             if "這份文件是可播放的文字動畫".encode("big5-uao") in data:
                 log.info("skip animation")
                 self.send("n")
                 is_animated = True
-        self.unt("瀏覽 第", on_data=on_data)
+        self.unt(self.on_first_col, on_data=handle_animated)
+        
         if is_animated:
             self.send("hq")
-            self.unt("瀏覽 第")
+            self.unt(self.on_first_col)
             log.info("refresh animation page")
+            
         if not self.article_configured:
             self.update_article_config()
+            
         log.info("start collecting body")
+        
         article = Article()
-        rx_last_page = re.compile(r"瀏覽.+?\(100%\)".encode("big5-uao"))
+        # rx_last_page = re.compile(r"瀏覽.+?\(100%\)".encode("big5-uao"))
         while True:
-            line_start, line_end = article.add_screen(self.lines(raw=True))
-            log.info("add screen {}~{}".format(line_start, line_end))
-            # if line_start == 308:
-                # set_trace()
-            if rx_last_page.search(self.get_line(self.screen.lines - 1)):
+            screen = article.add_screen(self.lines(raw=True))
+            log.info("add screen %s~%s", screen.line_start, screen.line_end)
+            
+            indent = 0
+            while any(line.right_truncated for line in screen.lines):
+                indent += 1
+                self.send(">")
+                self.unt(self.on_col(screen.col_start + 8))
+                screen = article.add_screen(self.lines(raw=True))
+                log.info("move right to col %s", screen.col_start)
+                
+            if indent:
+                self.send("<" * indent)
+                self.unt(self.on_first_col)
+            
+            if self.on_last_page():
                 break
+                
             self.send(" ")
-            needle = "{}~{} 行".format(line_end + 1, line_end + self.screen.lines - 1)
-            needle = needle.encode("big5-uao")
-            def page_loaded(data):
-                last_line = self.get_line(-1)
-                return needle in last_line or rx_last_page.search(last_line)
-            self.unt(page_loaded)
+            self.unt(lambda _data: self.on_last_page() or self.on_line(screen.line_end))
         self.send("q")
+        
         log.info("get article success")
         return article.to_bytes()
         
-def get_text(b):
-    return re.sub().decode("big5-uao")
+    rx_last_page = re.compile(r"瀏覽.+?\(100%\)".encode("big5-uao"))
+    def on_last_page(self):
+        return self.rx_last_page.search(self.get_line(-1))
+        
+    def on_line(self, line_no):
+        needle = "{}~{} 行".format(line_no + 1, line_no + len(self.screen.lines) - 1)
+        needle = needle.encode("big5-uao")
+        return needle in self.get_line(-1)
         
 def main():
     parser = ArgumentParser(description="Backup PTT mail.")
-    parser.add_argument("--user", help="username, otherwise prompt for the value.")
-    parser.add_argument("--pass", dest="password", help="password, otherwise prompt for the value.")
-    parser.add_argument("--dest", default=".", help="save to dest. Default to current dir.")
+    parser.add_argument(
+        "--user", help="username, otherwise prompt for the value."
+    )
+    parser.add_argument(
+        "--pass", dest="password", help="password, otherwise prompt for the value."
+    )
+    parser.add_argument(
+        "--dest", default=".", help="save to dest. Default to current dir."
+    )
     range_group = parser.add_mutually_exclusive_group(required=True)
     range_group.add_argument(
         "--range", nargs=2, type=int, metavar=("START", "END"),
-        help="specify a range (inclusive). Negative values and zeros are allowed, they are treated as (last_index + value) i.e. --range 0 0 would download the last mail."
+        help="specify a range (inclusive). Negative values and zeros are "
+             "allowed, they are treated as (last_index + value) i.e. --range 0 "
+             "0 would download the last mail."
     )
     range_group.add_argument("--all", action="store_true", help="download all")
     args = parser.parse_args()
