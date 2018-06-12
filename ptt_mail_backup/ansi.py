eec505d7d0cd69c26e5f02ef29411e5d63171993
eight <eight04@gmail.com>
2018-06-13 03:49:39 +0800

    Add: refactor, support blink

--- ptt_mail_backup/ansi.py
+++ ptt_mail_backup/ansi.py
@@ -1,76 +1,63 @@
-import pyte.graphics
+from collections import namedtuple
+from pyte.graphics import FG_ANSI, BG_ANSI
 
-fg2code = {name: str(key).encode("latin-1") for key, name in pyte.graphics.FG_ANSI.items()}
-bg2code = {name: str(key).encode("latin-1") for key, name in pyte.graphics.BG_ANSI.items()}
+fg2code = {name: str(key).encode("latin-1") for key, name in FG_ANSI.items()}
+bg2code = {name: str(key).encode("latin-1") for key, name in BG_ANSI.items()}
 
-fg2code.update({"default": b"37"})
-bg2code.update({"default": b"40"})
+RESET = b"\x1b[m"
+
+ColorState = namedtuple("ColorState", ["bold", "blink", "fg", "bg"])
+
+DEFAULT_COLOR = ColorState(False, False, "white", "black")
+
+def char_to_color(char):
+    fg = char.fg if char.fg != "default" else "white"
+    bg = char.bg if char.bg != "default" else "black"
+    if char.reverse:
+        fg, bg = bg, fg
+    return ColorState(char.bold, char.blink, fg, bg)        
 
 def code_to_ansi(codes):
     return b"\x1b[" + b";".join(codes) + b"m"
-
-class ColorState:
-    RESET = b"\x1b[m"
     
-    def __init__(self, init=None):
-        self.bold = False
-        self.fg = "default"
-        self.bg = "default"
+def diff(color, other):
+    """Rerturn an escape sequence that would transform color state from
+    ``other`` to ``color``.
+    """
+    if color == DEFAULT_COLOR:
+        return RESET # reset
+    codes = []
+    if (color.bold != other.bold and not color.bold or
+            color.blink != other.blink and not color.blink):
+        # reset
+        codes.append(b"0")
+        other = DEFAULT_COLOR
         
-        if init:
-            self.bold = init.bold
-            self.fg = init.fg
-            self.bg = init.bg
-            
-    def __eq__(self, other):
-        return (
-            self.bold == other.bold and
-            self.fg == other.fg and
-            self.bg == other.bg
-        )
-    
-    def is_default(self):
-        return not self.bold and self.fg == "default" and self.bg == "default"
+    if color.bold and color.bold != other.bold:
+        codes.append(b"1")
+    if color.blink and color.blink != other.blink:
+        codes.append(b"5")
+    if color.fg != other.fg:
+        codes.append(fg2code[color.fg])
+    if color.bg != other.bg:
+        codes.append(bg2code[color.bg])
+    return code_to_ansi(codes)
         
-    def diff(self, other):
-        if self.is_default():
-            return ColorState.RESET # reset
-        codes = []
-        if self.bold != other.bold:
-            if not self.bold: # remove bold
-                codes.append(b"0")
-                codes.append(fg2code[self.fg])
-                codes.append(bg2code[self.bg])
-                return code_to_ansi(codes)
-            codes.append(b"1")
-        if self.fg != other.fg:
-            codes.append(fg2code[self.fg])
-        if self.bg != other.bg:
-            codes.append(bg2code[self.bg])
-        return code_to_ansi(codes)
-            
-    def gen(self, bg=None, fg=None, bold=None):
-        if bg is None:
-            bg = self.bg
-        if fg is None:
-            fg = self.fg
-        if bold is None:
-            bold = self.bold
-        return self.bg
-
 def colored_sequence(chars):
-    color = ColorState()
+    color = DEFAULT_COLOR
     for c in chars:
         if c is None:
             yield b" "
             continue
-        next_color = ColorState(c)
+        next_color = char_to_color(c)
         if color != next_color:
-            yield next_color.diff(color)
+            yield diff(next_color, color)
             color = next_color
         yield c.data.encode("latin-1")
-    if not color.is_default():
-        yield ColorState.RESET
+    if color != DEFAULT_COLOR:
+        yield RESET
         
 def chars_to_bytes(chars):
+    """Convert a list of :class:`pyte.screens.Char` into ansi escape sequence.
+    """
     return b"".join(colored_sequence(chars))
