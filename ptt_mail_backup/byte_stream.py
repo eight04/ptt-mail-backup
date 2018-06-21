a9d529c70bbaacc390eb9240edf70f969b1adc70
eight <eight04@gmail.com>
2018-06-22 01:27:53 +0800

    Change: switch to local pyte

--- ptt_mail_backup/byte_stream.py
+++ ptt_mail_backup/byte_stream.py
@@ -2,17 +2,17 @@
 A pyte stream that would work with bytes.
 """
 import re
-import pyte
+from .pyte import ByteStream as _ByteStream
 
 def no_c1_pattern(rx):
     # pyte uses a single backslash to escape every characters
     pattern = re.sub(r"\\[\x9b\x9d]", "", rx.pattern)
     return re.compile(pattern)
         
-class ByteStream(pyte.ByteStream):
+class ByteStream(_ByteStream):
     # pylint: disable=protected-access
     # https://github.com/selectel/pyte/issues/118
-    _text_pattern = no_c1_pattern(pyte.ByteStream._text_pattern)
+    _text_pattern = no_c1_pattern(_ByteStream._text_pattern)
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
